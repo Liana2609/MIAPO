f05c9dacbde8877e58ac254b42e576c05a516e76
Nikita Mamichev <mamichevnikita@gmail.com>
2023-07-05 10:17:55 +0300

    Добавьте файлы проекта.

---
+++ цветы консоль/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using цветы;
+
+namespace цветы_консоль
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Биктимирова Лиана
+            Garden gr = new Garden("г. Москва", "Биктимирова Л.Р.");
+            gr.ShowAll();
+
+            Flower f1 = new Flower("Глоксиния", "род растений семейства Геснериевые.\n Назван в честь немецкого или эльзасского ботаника и врача Беньямина Петер\n Глоксина.");
+            gr.Flowers.Add(f1);
+
+            Flower f2 = new Flower("Ромашка", "род однолетних цветковых растений\n семейства астровые, или сложноцветные, по современной классификаци\nобъединяет около 70 видовневысоких пахучих трав, цветущих с первого год\n жизни.");
+            gr.Flowers.Add(f2);
+
+            Flower f3 = new Flower("Роза", "собирательное название видов и сортов\n представителей рода Шиповник, выращиваемых человеком и растущих в дикой\n природе.");
+            gr.Flowers.Add(f3);
+
+            Flower f4 = new Flower("Незабудка", "род травянистых растений семейства Бурачниковые. Некоторые виды культивируют в качестве красивоцветущих садовых растений.");
+            gr.Flowers.Add(f4);
+
+            Flower f5 = new Flower("Анютины глазки", "травянистое однолетнее или\n двулетнее растение, распространённое в Европе и умеренных областях Азии;\n вид рода Фиалка семейства Фиалковые.");
+            gr.Flowers.Add(f5);
+
+            gr.Flowers.Sort();
+            foreach (Flower f in gr.Flowers)
+            {
+                f.Show();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ цветы/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цветы
+{
+    //Биктимирова  Лиана
+        // класс - сад
+        public class Garden
+        {
+    
+            public string Address { get; set; }
+            public string Owner { get; set; }
+            public List<Flower> Flowers = new List<Flower>();
+            public Garden(string ad, string ow)
+            {
+                Address = ad;
+                Owner = ow;
+            }
+            public void ShowAll()
+            {
+                Console.WriteLine($"Сад: адрес = {Address},\n владелец = {Owner}");
+            }
+        }
+    //Биктимирова Лиана
+    //  класс цветы
+    public class Flower : IComparable<Flower>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Flower(string nm, string dsc)
+        {
+            Title = nm;
+            Description = dsc;
+        }
+        public void Show()
+        {
+            Console.WriteLine(
+                 $"Цветок: название = {Title},\n описание = {Description}");
+        }
+        public int CompareTo(Flower other)
+        {
+            if (this.Title.Length > other.Title.Length) return 1; 
+            if (this.Title.Length < other.Title.Length) return -1; 
+            else return 0;
+        }
+    }
+}
